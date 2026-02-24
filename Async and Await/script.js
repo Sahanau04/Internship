b1cd37508f0098a2371f05f442a0bd933f554acd
Sahanau04 <usahana250@gmail.com>
2026-02-24 12:09:35 +0530

    readme.md

---
+++ Async and Await/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", getData);
+
+async function getData() {
+    try {
+        output.innerHTML = "Loading...";
+
+        // Small delay to show async behavior
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        output.innerHTML = 
+            "Name: Sahana U <br>" +
+            "Email: usahana250@gmail.com <br>" +
+            "City: Sagara";
+
+    } catch (error) {
+        output.innerHTML = "Error loading data!";
+    }
+}
